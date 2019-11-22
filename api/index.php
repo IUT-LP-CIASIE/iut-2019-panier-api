4440e90411b79c61966ee3e68f791f980b794a6a
SOPRESS <gilles@sopress.net>
2019-11-22 09:23:57 +0100

    token

--- api/index.php
+++ api/index.php
@@ -2,9 +2,15 @@
 
 session_start();
 require_once '../vendor/autoload.php';
-
+$token=false;
 if(isset($_GET['token'])) {
-	$token = md5($_GET['token']);
+	$token = $_GET['token'];
+	if($$token == '[votre adresse mail ici!]'){
+		$$token = '';
+	}
+}
+if($token) {
+	$token = md5($token);
 } else {
 	die('You must provide a token via a GET parameter to all API calls');	
 }
@@ -15,8 +21,8 @@ $app = new \Slim\App();
 $app->add(function(ServerRequestInterface $request, ResponseInterface $response, callable $next){
 	$response = $response->withHeader('Content-type', 'application/json; charset=utf-8');
 //	$response = $response->withHeader('Access-Control-Allow-Origin', 'http://allweb.fun');
-  $response = $response->withHeader('Access-Control-Allow-Origin', '*');
-$response = $response->withHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+	$response = $response->withHeader('Access-Control-Allow-Origin', '*');
+	$response = $response->withHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
 	return $next($request, $response);
 });
 
@@ -99,13 +105,13 @@ $app->group('/cart', function() use($app, $products){
  *
  * @apiSuccess {Array} cart The cart content
  */
- 	$app->get('', function() {
-		$cart = file_get_contents($GLOBALS['file_cart']);
-		if(!$cart) {
-			$cart='[]';
-		}
-		echo $cart;
-	});
+$app->get('', function() {
+	$cart = file_get_contents($GLOBALS['file_cart']);
+	if(!$cart) {
+		$cart='[]';
+	}
+	echo $cart;
+});
 
 
 /**
@@ -117,20 +123,20 @@ $app->group('/cart', function() use($app, $products){
  *
  * @apiSuccess {Array} cart The cart content
  */
-	$app->delete('/{pid}', function(Slim\Http\Request $request, Slim\Http\Response $response) {
-		$pid = $request->getAttribute('pid');
+$app->delete('/{pid}', function(Slim\Http\Request $request, Slim\Http\Response $response) {
+	$pid = $request->getAttribute('pid');
 
-		$current = getCart();
-		$final = [];
-		foreach($current as $k=>$v) {
-				if($v['id'] != $pid){
-					$final[$k]=$v;
-				}
+	$current = getCart();
+	$final = [];
+	foreach($current as $k=>$v) {
+		if($v['id'] != $pid){
+			$final[$k]=$v;
 		}
-		$json = json_encode(array_values($final));
-		file_put_contents($GLOBALS['file_cart'],$json);
-		echo $json;
-	});
+	}
+	$json = json_encode(array_values($final));
+	file_put_contents($GLOBALS['file_cart'],$json);
+	echo $json;
+});
 
 /**
  * @api {delete} /api/cart Empty cart
@@ -139,10 +145,10 @@ $app->group('/cart', function() use($app, $products){
  *
  * @apiSuccess {Array} cart The cart content (an empty array)
  */
-	$app->delete('', function() {
-		file_put_contents($GLOBALS['file_cart'],'');
-		echo '[]';
-	});
+$app->delete('', function() {
+	file_put_contents($GLOBALS['file_cart'],'');
+	echo '[]';
+});
 
 
 /**
@@ -154,26 +160,26 @@ $app->group('/cart', function() use($app, $products){
  *
  * @apiSuccess {Array} cart The cart content
  */
-	$app->post('/{pid}', function(Slim\Http\Request $request, Slim\Http\Response $response) use($products){
-		$pid = $request->getAttribute('pid');
-		if(!empty($pid) && isset($products[$pid])){
-			$current = getCart();
-			if( ! isset($current[$pid]) ){
-				$current[$pid]['id'] = $products[$pid]['id'];
-				$current[$pid]['nom'] = $products[$pid]['nom'];
-				$current[$pid]['qte'] = 0;
-				$current[$pid]['ok'] = false;
-				$current[$pid]['prix'] = 0;
-			}
-
-			$current[$pid]['qte'] += 1;
-			$current[$pid]['prix'] = $current[$pid]['qte'] * $products[$pid]['prix'];
-
-			$to_json = json_encode(array_values($current));
-			file_put_contents($GLOBALS['file_cart'],$to_json);
-			echo $to_json;
+$app->post('/{pid}', function(Slim\Http\Request $request, Slim\Http\Response $response) use($products){
+	$pid = $request->getAttribute('pid');
+	if(!empty($pid) && isset($products[$pid])){
+		$current = getCart();
+		if( ! isset($current[$pid]) ){
+			$current[$pid]['id'] = $products[$pid]['id'];
+			$current[$pid]['nom'] = $products[$pid]['nom'];
+			$current[$pid]['qte'] = 0;
+			$current[$pid]['ok'] = false;
+			$current[$pid]['prix'] = 0;
 		}
-	});
+
+		$current[$pid]['qte'] += 1;
+		$current[$pid]['prix'] = $current[$pid]['qte'] * $products[$pid]['prix'];
+
+		$to_json = json_encode(array_values($current));
+		file_put_contents($GLOBALS['file_cart'],$to_json);
+		echo $to_json;
+	}
+});
 
 
 /**
@@ -185,8 +191,8 @@ $app->group('/cart', function() use($app, $products){
  *
  * @apiSuccess {Object} ressource A structured ressource containing the state of the product's order
  */
- 	$app->put('/{pid}/buy', function(Slim\Http\Request $request, Slim\Http\Response $response){
-		$pid = $request->getAttribute('pid');
+$app->put('/{pid}/buy', function(Slim\Http\Request $request, Slim\Http\Response $response){
+	$pid = $request->getAttribute('pid');
 		sleep(rand(0,5)); // faire durer, de manière aléatoire, le temps de traitement de cette méthode
 		echo json_encode(array('success' => true, 'product' => $pid));
 	});
